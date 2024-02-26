a78e7b20f2dd19c3934feade09ff521fa0233feb
Danny Ryan <dannykryan@gmail.com>
2024-02-26 16:09:03 +0000

    updated button hover behavior style indesign system

--- src/Components/ConfirmationModal.tsx
+++ src/Components/ConfirmationModal.tsx
@@ -15,13 +15,13 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
     <div className={`modal-container fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50 ${isOpen ? 'block' : 'hidden'}`}>
       <div className="bg-white p-6 rounded-lg shadow-lg">
         <p>{message}</p>
-        <div className="flex justify-end space-x-4 mt-4">
-          <button onClick={onClose} className="px-4 py-2 bg-gray-500 text-white rounded-md">Cancel</button>
-            <button onClick={() => {
+        <div className="flex justify-center mt-4">
+        <button onClick={() => {
               onClose(); // Optionally close the modal after confirmation
-            }} className="px-4 py-2 bg-blue-500 text-white rounded-md">
+            }} className="btn">
               Confirm
             </button>
+          <button onClick={onClose} className="btn btn-reject">Cancel</button>
         </div>
       </div>
     </div>
